a06ce9ecec3e4f7a0d1f7fa02d7ad19555f7716a
Patrick Mutwiri <patwiri@gmail.com>
2020-03-12 07:22:47 +0300

    DT Dobie. q2

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Vehicle {
+    public:
+    string make,model,engine_number;
+    double price;
+
+
+
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
