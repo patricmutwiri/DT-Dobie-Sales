8f072fde272ca79f5dcfe8781f2d3e1755c2f2b7
Patrick Mutwiri <patwiri@gmail.com>
2020-03-12 10:58:50 +0300

    final

--- main.cpp
+++ main.cpp
@@ -5,14 +5,42 @@ using namespace std;
 class Vehicle {
     public:
     string make,model,engine_number;
-    double price;
+    double price=0.0, profit_pc = 0.15, profit=0.0;
 
+    void set_vehicle(){
+        cout<<"\nVehicle Details entry. ";
+        cout<<"\nEnter vehicle make : ";
+        cin>>make;
+        cout<<"\nEnter vehicle model : ";
+        cin>>model;
+        cout<<"\nEnter vehicle price : ";
+        cin>>price;
+        cout<<"\n\n";
+    }
 
-
-}
+    void get_profit(){
+        profit = price*profit_pc;
+        cout<<"\n:: Vehicle Details ::";
+        cout<<"\nVehicle Make \t: "<<make;
+        cout<<"\nVehicle Model \t: "<<model;
+        cout<<"\nVehicle Price \t: "<<price;
+        cout<<"\nVehicle Profit \t: "<<profit;
+        cout<<"\n";
+    }
+};
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "\n\n";
+    cout << "\n\nDT DOBIE Sales(K) Ltd Sales System " << endl;
+    cout << "\n";
+
+    // Get Vehicle details
+    Vehicle vh;
+    vh.set_vehicle();
+
+    // Get vehicle sales profit
+    vh.get_profit();
+
     return 0;
 }
